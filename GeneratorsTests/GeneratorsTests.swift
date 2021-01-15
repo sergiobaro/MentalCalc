87a2571a9b76ecf96c2a1363cfc8df8961a29fab
Sergio Baró <serbarsim@gmail.com>
2021-01-15 10:42:36 +0100

    Generated questions have selected number as the right argument

--- GeneratorsTests/GeneratorsTests.swift
+++ GeneratorsTests/GeneratorsTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class GeneratorsTests: XCTestCase {
 
-  func test() {
+  func test_idsAreUnique() {
     let generator = SelectionTableQuizGenerator(maxNumber: 12, numbers: Set<Int>(arrayLiteral: 5, 6))
     let questions = generator.generateAll()
 
@@ -14,4 +14,14 @@ class GeneratorsTests: XCTestCase {
     XCTAssertEqual(idsSet.count, 24)
   }
 
+  func test_randomNumbersOnTheLeft() {
+    let generator = SelectionTableQuizGenerator(maxNumber: 12, numbers: Set<Int>(arrayLiteral: 11))
+    let questions = generator.generateAll()
+
+    XCTAssertEqual(questions.count, 12)
+
+    questions.forEach {
+      XCTAssertEqual($0.right, 11)
+    }
+  }
 }
